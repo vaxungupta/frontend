5fbbdfee91422b2e58217ca175a82a740f5407d9
vaxungupta <varungupta69331@gmail.com>
2025-02-21 18:30:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React, { useState } from "react";
+import InputForm from "./components/InputForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REST API Frontend</h1>
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
 
